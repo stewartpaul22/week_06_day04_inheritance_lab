be5629f62434fa7f2213aca457b50270068a9f51
Paul Stewart <stewartpaul22@yahoo.co.uk>
2018-03-01 16:32:47 +0000

    Add Employee class with getters, raiseSalary() and payBonus().

--- src/main/java/Employee.java
+++ src/main/java/Employee.java
@@ -1,2 +1,34 @@
 public class Employee {
+
+    //name, NI number and salary
+    private String name;
+    private String niNumber;
+    private Double salary;
+
+    public Employee(String name, String niNumber, double salary) {
+        this.name = name;
+        this.niNumber = niNumber;
+        this.salary = salary;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getNiNumber() {
+        return niNumber;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void raiseSalary(Double increment) {
+        this.salary *= increment;
+    }
+
+    //Add a method called payBonus which returns 1% of the employees salary
+    public Double payBonus() {
+        return (this.salary * 0.01);
+    }
 }
--- src/test/java/EmplyeeTest.java
+++ src/test/java/EmplyeeTest.java
@@ -1,2 +1,12 @@
+import org.junit.Before;
+
 public class EmplyeeTest {
+
+    private Employee employee;
+
+    @Before
+    public void before() {
+        employee = new Employee("Bob", "LN655443T", 30000.00);
+    }
+
 }
